0a7bf039928b9103db7fa3e7d2440748be713422
AIsaac10 <isaacmoreira12332@gmail.com>
2025-09-28 19:23:24 -0300

    add routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PlantController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/plants",[PlantController::class, "index"])->name("plants.index");
+Route::get("/plants/create",[PlantController::class, "create"])->name("plants.create");
+Route::post("/plants",[PlantController::class, "store"])->name("plants.store");
+Route::get("/plants{plant}",[PlantController::class, "show"])->name("plants.show");
+Route::get("/plants{plant}/edit",[PlantController::class, "edit"])->name("plants.edit");
+Route::put("/plants{plant}",[PlantController::class, "update"])->name("plants.update");
+Route::delete("/plants{plant}",[PlantController::class, "destroy"])->name("plants.destroy");
