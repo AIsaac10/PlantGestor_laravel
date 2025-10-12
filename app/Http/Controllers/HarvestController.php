7ed51c23e99922262db1e2b86a41becfc18a3f32
AIsaac10 <isaacmoreira12332@gmail.com>
2025-10-12 16:13:32 -0300

    feat harvest index

--- app/Http/Controllers/HarvestController.php
+++ app/Http/Controllers/HarvestController.php
@@ -3,15 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Harvest;
 
 class HarvestController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+
     public function index()
     {
-        //
+        $harvests = Harvest::all();
+        return view("harvests.index", compact("harvests"));
     }
 
     /**
---
+++ resources/views/harvests/index.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Plantas</title>
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+</head>
+<body>
+    <header>
+        <h1>Colheita</h1>
+    </header>
+    
+        <a id="linkCreate" href="{{ route('harvests.create') }}">Criar novo Cadastro</a>
+
+
+
+    <div id="container">
+        <table border="1" cellpadding="5" cellspacing="0">
+            <thead>
+                <tr>
+                    <th>Colheita</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($harvests as $harvest)
+                    <tr>
+                        <td>{{ $harvest->culture }}
+                            
+                        </td>
+                        <td>
+                            <a class="btnEdit" href="{{ route('harvests.edit', $harvest->id) }}">Editar</a> 
+                            
+                            
+                            <form action="{{ route('harvests.destroy', $harvest->id) }}" method="POST" style="display:inline" onsubmit="return confirm('Tem certeza que deseja excluir este registro?')">
+                                @csrf
+                                @method("DELETE")
+                                <input class="btnDelete" type="submit" value="Excluir">
+                            
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</body>
