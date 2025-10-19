79c06a00af3e8a25e8e7c615e89230190019ae39
AIsaac10 <isaacmoreira12332@gmail.com>
2025-10-19 16:40:02 -0300

    fix form create harvest

--- resources/views/harvests/create.blade.php
+++ resources/views/harvests/create.blade.php
@@ -6,17 +6,26 @@
     <title>Document</title>
     <link rel="stylesheet" href="{{ asset('css/form.css') }}">
 </head>
-<body>
-    <h1>Cadastro de Plantas</h1>
+    <body>
+        <h1>Cadastro de Plantas</h1>
 
-    <form action="{{route('harvests.store')}}" method="post">
-        @csrf
-        <input type="text" value="{{ $plant->id }}">
-        <label for="tempoColheita">Tempo de Colheita:</label>
-        <input type="date" name="tempoColheita" required> 
-        <label for="tempoColheita">Tempo de Colheita:</label>
-        <input type="date" name="tempoColheita" required> 
-        <input type="submit" value="Enviar">
-    </form>
-</body>
+        <form action="{{ route('harvests.store') }}" method="POST">
+            @csrf
+
+            <label for="culture">Planta:</label>
+            <select name="culture" id="culture" required onchange="updateCulture()">
+                @foreach($plants as $plant)
+                    <option value="{{ $plant->culture }}">{{ $plant->culture }}</option>
+                @endforeach
+            </select>
+
+            <label for="time_harvest">Horário da Colheita:</label>
+            <input type="text" name="time_harvest" id="time_harvest" required>
+
+            <label for="weight_harvest">Peso:</label>
+            <input type="number" step="0.01" name="weight_harvest" id="weight_harvest" required>
+
+            <input type="submit" value="Cadastrar">
+        </form>
+    </body>
 </html>
