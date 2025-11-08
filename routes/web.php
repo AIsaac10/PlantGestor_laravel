c6f97aed5e7509b36fc678736ff55b1aa9394b6d
AIsaac10 <isaacmoreira12332@gmail.com>
2025-11-08 00:25:13 -0300

    feat fertilizer routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PlantController;
 use App\Http\Controllers\HarvestController;
+use App\Http\Controllers\FertilizerController;
 
 Route::get("/",[PlantController::class, "index"]);
 
@@ -21,3 +22,11 @@ Route::get("/harvests/{harvest}",[HarvestController::class, "show"])->name("harv
 Route::get("/harvests/{harvest}/edit",[HarvestController::class, "edit"])->name("harvests.edit");
 Route::put("/harvests/{harvest}",[HarvestController::class, "update"])->name("harvests.update");
 Route::delete("/harvests/{harvest}",[HarvestController::class, "destroy"])->name("harvests.destroy");
+
+Route::get("/fertilizers",[FertilizerController::class, "index"])->name("fertilizers.index");
+Route::get("/fertilizers/create",[FertilizerController::class, "create"])->name("fertilizers.create");
+Route::post("/fertilizers",[FertilizerController::class, "store"])->name("fertilizers.store");
+Route::get("/fertilizers/{fertilizer}",[FertilizerController::class, "show"])->name("fertilizers.show");
+Route::get("/fertilizers/{fertilizer}/edit",[FertilizerController::class, "edit"])->name("fertilizers.edit");
+Route::put("/fertilizers/{fertilizer}",[FertilizerController::class, "update"])->name("fertilizers.update");
+Route::delete("/fertilizers/{fertilizer}",[FertilizerController::class, "destroy"])->name("fertilizers.destroy");
