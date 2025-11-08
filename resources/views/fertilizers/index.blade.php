5350b5f83af0d19077686e5bab75169174768a56
AIsaac10 <isaacmoreira12332@gmail.com>
2025-11-08 00:45:52 -0300

    feat style and structure in fertilizer

--- resources/views/fertilizers/index.blade.php
+++ resources/views/fertilizers/index.blade.php
@@ -3,9 +3,77 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Plantas</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body>
+<body class="bg-gray-50">
+    <header class="bg-green-500 flex p-3">
+        <h1 class="text-2xl pr-6 text-white">PlantGestor</h1>
+        <a class="inline-block bg-gray-800 hover:bg-gray-900 text-white text-sm px-3 py-1 rounded transition" href="{{ route('plants.index') }}">Plantas</a>
+        <a class="inline-block bg-gray-800 hover:bg-gray-900 text-white text-sm px-3 py-1 rounded transition" href="{{ route('harvests.index') }}">Colheitas</a>
+        <a class="inline-block bg-gray-800 hover:bg-gray-900 text-white text-sm px-3 py-1 rounded transition" href="{{ route('fertilizers.index') }}">Fertilizantes</a>
+    </header>
 
+    <div class="flex items-center">
+        <div class="p-5 pr-95">
+            <a class=" inline-block bg-gray-800 hover:bg-gray-900 text-white text-sm px-5 py-3 rounded transition" href="{{ route('plants.create') }}">Criar novo Cadastro</a>
+        </div>
+
+        @if (session('success'))
+        <div id="alert-message" class="bg-green-100 text-green-800 border border-green-300 px-4 py-2 rounded-md shadow-sm font-medium transition-opacity duration-500">
+            {{ session('success') }}
+        </div>
+
+        <script>
+
+            setTimeout(() => {
+                const alert = document.getElementById('alert-message');
+                if (alert) {
+                    alert.style.opacity = '0';
+                    setTimeout(() => alert.remove(), 500);
+                }
+            }, 3000);
+        </script>
+        @endif
+        
+    </div>
+
+
+
+    <div class="overflow-x-auto flex items-center justify-center">
+        <table class="min-w-200 border border-gray-200 rounded-lg shadow-sm overflow-hidden">
+            <thead class="bg-green-600 text-white">
+                <tr>
+                    <th class="px-6 py-3 text-left text-sm font-semibold">Nome da Cultura</th>
+                    <th class="px-6 py-3 text-left text-sm font-semibold">Nome do fertilizante</th>
+                    <th class="px-6 py-3 text-left text-sm font-semibold">Data de fertilização</th>
+                    <th class="px-6 py-3 text-left text-sm font-semibold">Quantidade de fertilizante</th> 
+                    <th class="px-6 py-3 text-left text-sm font-semibold">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($fertilizers as $fertilizer)
+                    <tr class="odd:bg-white even:bg-gray-100">
+                        <td class="px-6 py-3 border-t border-gray-200 text-gray-700">{{ $fertilizer->culture }}</td>
+                        <td class="px-6 py-3 border-t border-gray-200 text-gray-700">{{ $fertilizer->fertilizer }}</td>
+                        <td class="px-6 py-3 border-t border-gray-200 text-gray-700">{{ $fertilizer->time_fertilizer }}</td>
+                        <td class="px-6 py-3 border-t border-gray-200 text-gray-700">{{ $fertilizer->weight_fertilizer }}</td>
+
+                        <td class="px-6 py-3 border-t border-gray-200 space-x-2">
+                            <a class="inline-block bg-gray-800 hover:bg-gray-900 text-white text-sm px-3 py-1 rounded transition" href="{{ route('fertilizers.edit', $fertilizer->id) }}">Editar</a> 
+                            
+                            
+                            <form class="bg-red-500 hover:bg-red-600 text-white text-sm px-3 py-1 rounded cursor-pointer transition" action="{{ route('fertilizers.destroy', $fertilizer->id) }}" method="POST" style="display:inline" onsubmit="return confirm('Tem certeza que deseja excluir este registro?')">
+                                @csrf
+                                @method("DELETE")
+                                <input class="btnDelete" type="submit" value="Excluir">
+                            
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
