5108a8a87709003e9c426c995c9134bfb34e7f19
AIsaac10 <isaacmoreira12332@gmail.com>
2025-11-16 21:04:50 -0300

    feat structure fertilizer create

--- resources/views/fertilizers/create.blade.php
+++ resources/views/fertilizers/create.blade.php
@@ -1,1 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Fertilizantes</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="bg-gray-100 flex flex-col items-center justify-center min-h-screen">
 
+    <div class="bg-white p-6 rounded-xl shadow-lg w-full max-w-sm">
+        <h1 class="text-xl font-semibold text-gray-800 mb-4 text-center">
+            Cadastro de Fertilizantes
+        </h1>
+
+        <form action="{{ route('fertilizers.store') }}" method="POST" class="space-y-4">
+            @csrf
+
+            <div>
+                <label for="culture" class="block text-gray-700 font-medium mb-1">Planta:</label>
+                <select name="culture" id="culture" required onchange="updateCulture()">
+                    @foreach($plants as $plant)
+                        <option value="{{ $plant->culture }}">{{ $plant->culture }}</option>
+                    @endforeach
+                </select>
+                
+                <label for="fertilizer" class="block text-gray-700 font-medium mb-1">
+                    Fertilizante:
+                </label>
+                <input type="text" id="fertilizer" name="fertilizer" required
+                class="w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-green-500 text-gray-800">
+
+                <label for="time_fertilizer" class="block text-gray-700 font-medium mb-1">
+                    Data da Fertilização:
+                </label>
+                <input type="text" id="time_fertilizer" name="time_fertilizer" required
+                class="w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-green-500 text-gray-800">
+
+                <label for="weight_fertilizer" class="block text-gray-700 font-medium mb-1">
+                    Fertilizante:
+                </label>
+                <input type="number" id="weight_fertilizer" name="weight_fertilizer" required
+                class="w-full border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-green-500 text-gray-800">
+            </div>
+
+            <button type="submit"
+                class="w-full bg-gray-800 hover:bg-gray-900 text-white font-medium py-2 rounded-md transition">
+                Enviar
+            </button>
+        </form>
+    </div>
+    <a class="inline-block  hover:text-gray-500 text-bg-gray-800 text-sm px-3 py-1 rounded transition" href="{{ route('plants.index') }}">Retornar</a>
+</body>
+</html>
